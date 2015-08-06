e893294c1eb44c7f2d2393197c61a758c4dc5ef4
Kyung Mo, Kim <kkim@securityplatform.co.kr>
2015-08-06 13:28:07 +0900

    determine i2c bus number according to /sys/devices/virtual/dmi/id/board_name

--- meta-sp/recipes-support/ii2c/files/ii2c.c
+++ meta-sp/recipes-support/ii2c/files/ii2c.c
@@ -2,35 +2,58 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
-int i2c_n_exists(int n) {
-    int r;
-    char fnamebuf[16];
+#define PATH_SYS_BOARD_NAME "/sys/devices/virtual/dmi/id/board_name"
+
+int read1line(char *path, char *buf, int len)
+{
+    FILE *f;
+    int l;
+    f = fopen(path, "r");
+    if (f == NULL) {
+	buf[0] = '\0';
+	return -1;
+    }
+    l = fread(buf, 1, len, f);
+    fclose(f);
+    if (l <= 0) {
+	return -1;
+    }
+    return 0;
+}
+
+int find_i2cbus(void)
+{
     struct stat statbuf;
-
-    if (n < 0 || n > 10) {
+    char boardname[20];
+    const char gal2 [] = "GalileoGen2";
+    const char edison1[] = "BODEGA BAY";
+    const char edison2[] = "SALT BAY";
+    int r = stat(PATH_SYS_BOARD_NAME, &statbuf);
+    if (r < 0) {
 	return 0;
     }
-    snprintf(fnamebuf, 15, "/dev/i2c-%d", n);
-    r = stat(fnamebuf, &statbuf);
-    if (r == 0) {
-	return 1;
+    r = read1line(PATH_SYS_BOARD_NAME, boardname, 20);
+    if (r < 0) {
+	return 0;
+    }
+    if (strncmp(boardname, gal2, strlen(gal2)) == 0) {
+	return 0;
+    } else if (strncmp(boardname, edison1, strlen(edison1)) == 0) {
+	return 6;
+    } else if (strncmp(boardname, edison2, strlen(edison2)) == 0) {
+	return 6;
     }
     return 0;
 }
 
 int main(void)
 {
-    mraa_i2c_context i2c;
-    mraa_result_t r;
+    int i2cbus = find_i2cbus();
 
-    int ns[] = {0, 6, -1};
-    int i; 
-    
-    for (i=0; ns[i] > 0; ++i) {
-	if (i2c_n_exists(ns[i])) {
-	    i2c = mraa_i2c_init(ns[i]);
-	}
-    }
-
+    mraa_init();
+    mraa_i2c_init(i2cbus);
+    return 0;
 }
